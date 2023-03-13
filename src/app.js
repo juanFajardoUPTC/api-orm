d4632e9efe0db979830fca15b3f3f480e2b87b72
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-03-12 21:15:35 -0500

    revisar
    revisar

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const path = require("path");
 const app = express();
-
+const { PrismaClient } = require("@prisma/client");
+const prisma =new PrismaClient()
 
 //config
 app.set('port', process.env.PORT || 3000);
@@ -13,8 +14,31 @@ app.use(express.static(path.join(__dirname,'public')));
 
 //start
 app.get("/",(req,res)=>{
-    res.json({msg:"hola"})
+    res.json({msg:"hola perra"})
 })
+
+app.post('/estudiantes', async (req, res) => {
+    try {
+      const estudiante = await prisma.estudiantes.create({
+        data: {
+          codigo: req.body.codigo,
+          nombre: req.body.nombre,
+          apellido: req.body.apellido,
+          tipo_documento: req.body.tipo_documento,
+          numero_documento: req.body.numero_documento,
+          estado: req.body.estado,
+          genero: req.body.genero
+        }
+      });
+      res.json(estudiante);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error al crear el estudiante');
+    }
+  });
+
+
+
 const server = app.listen(app.get('port'),()=>{
     console.log('Funciona en puerto: ', app.get('port'));
 });
