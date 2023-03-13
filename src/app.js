c1e911dec9adacedcdeedb17a6f8e0554518d836
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-03-12 22:09:29 -0500

    daniel

--- src/app.js
+++ src/app.js
@@ -16,28 +16,52 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get("/",(req,res)=>{
     res.json({msg:"hola perra"})
 })
-
+// Crear un nuevo estudiante
 app.post('/estudiantes', async (req, res) => {
-    try {
-      const estudiante = await prisma.estudiantes.create({
-        data: {
-          codigo: req.body.codigo,
-          nombre: req.body.nombre,
-          apellido: req.body.apellido,
-          tipo_documento: req.body.tipo_documento,
-          numero_documento: req.body.numero_documento,
-          estado: req.body.estado,
-          genero: req.body.genero
-        }
-      });
-      res.json(estudiante);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error al crear el estudiante');
+  const nuevoEstudiante = await prisma.estudiantes.create({
+    data: {
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      tipo_documento: req.body.tipo_documento,
+      numero_documento: req.body.numero_documento,
+      estado: 1,
+      genero: req.body.genero
     }
   });
+  res.json(nuevoEstudiante);
+});
 
+// Editar un estudiante existente
+app.put('/estudiantes/:id', async (req, res) => {
+  const estudianteActualizado = await prisma.estudiantes.update({
+    where: { codigo: parseInt(req.params.id) },
+    data: {
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      tipo_documento: req.body.tipo_documento,
+      numero_documento: req.body.numero_documento,
+      genero: req.body.genero
+    }
+  });
+  res.json(estudianteActualizado);
+});
 
+// Desactivar un estudiante
+app.delete('/estudiantes/:id', async (req, res) => {
+  const estudianteDesactivado = await prisma.estudiantes.update({
+    where: { codigo: parseInt(req.params.id) },
+    data: {
+      estado: 0
+    }
+  });
+  res.json(estudianteDesactivado);
+});
+
+// Listar todos los estudiantes
+app.get('/estudiantes', async (req, res) => {
+  const estudiantes = await prisma.estudiantes.findMany();
+  res.json(estudiantes);
+});
 
 const server = app.listen(app.get('port'),()=>{
     console.log('Funciona en puerto: ', app.get('port'));
