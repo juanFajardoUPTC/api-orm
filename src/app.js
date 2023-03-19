805dfbc4fb029dd0979156084b35dd9310fbd2a4
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-03-18 23:12:52 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,75 +1,96 @@
 const express = require("express");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
+// const req = require("express/lib/request");
+// const res = require("express/lib/response");
 const path = require("path");
 const app = express();
-const { PrismaClient } = require("@prisma/client");
-const prisma =new PrismaClient()
+
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 
 //config
 app.set('port', process.env.PORT || 3000);
 
 //static
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //start
-app.get("/",(req,res)=>{
-    res.json({msg:"hola perra BEBE"})
-})
-// Crear un nuevo estudiante
-app.post('/estudiantes/crear', async (req, res) => {
+////obtener lista de estudantes ya esta listo
+app.get("/estudiantes", async (req, res) => {
     try{
-    console.log(req.body);
-    const { nombre, apellido, tipo_documento, numero_documento, estado, genero } = req.body;
-    const student = await prisma.estudiantes.create({
-        data:{
-            nombre,
-            apellido,
-            tipo_documento,
-            numero_documento,
-            estado,
-            genero
-        },
-    })
-    res.json({msg:"estudiante creado",estudiantes:student})
+        const estudiantes= await prisma.estudiantes.findMany();
+        res.json({ estudiantes })
     }catch(error){
         console.error(error);
-        res.status(500).json({ mensaje: 'Error al crear el estudiante' });
+        res.status(500).json({ mensaje: "Error al obtener la lista de estudiantes" });
     }
-});
+    
+})
 
-// Editar un estudiante existente
-app.put('/estudiantes/:id', async (req, res) => {
-  const estudianteActualizado = await prisma.estudiantes.update({
-    where: { codigo: parseInt(req.params.id) },
-    data: {
-      nombre: req.body.nombre,
-      apellido: req.body.apellido,
-      tipo_documento: req.body.tipo_documento,
-      numero_documento: req.body.numero_documento,
-      genero: req.body.genero
+//ya esta listo
+app.post("/estudiantes/agregar", async (req, res) => {
+    try {
+        console.log(req.body);
+        const estudiante = await prisma.estudiantes.create({
+            data: req.body
+        })
+        res.json({ msg: "creado", estudiante })
+    } catch (error) {
+        console.error(error);
+        // Si el error se debe a que se violó una restricción única, responder con un mensaje específico
+        if (error.code === "P2002") {
+            res.status(400).json({ mensaje: "Ya existe un estudiante con el mismo código o número de documento" });
+        } else {
+            res.status(500).json({ mensaje: "Error al crear el estudiante" });
+        }
     }
-  });
-  res.json(estudianteActualizado);
-});
+})
 
-// Desactivar un estudiante
-app.delete('/estudiantes/:id', async (req, res) => {
-  const estudianteDesactivado = await prisma.estudiantes.update({
-    where: { codigo: parseInt(req.params.id) },
-    data: {
-      estado: 0
+
+app.patch("/estudiantes/estado/:codigo", async(req,res) =>{
+    try{
+        console.log(req.body);
+        const codigo = Number(req.body.codigo);
+        const {estado}= req.body;
+        const estudiante= await prisma.estudiantes.update({
+            where:{ codigo },
+            data:{estado}
+        })
+        res.json({ msg: "estado actualizado", estudiante })
+    }catch (error){
+        console.error(error);
+        if (error.code === "P2025") {
+            res.status(404).json({ mensaje: `No se encontró un estudiante con el código ${req.params.codigo}` });
+        } else {
+         res.status(500).json({ mensaje: "Error al actualizar ESTADO el estudiante" });
+        }
     }
-  });
-  res.json(estudianteDesactivado);
-});
-
-// Listar todos los estudiantes
-app.get('/estudiantes', async (req, res) => {
-  const estudiantes = await prisma.estudiantes.findMany();
-  res.json(estudiantes);
-});
-
-const server = app.listen(app.get('port'),()=>{
+})
+app.put("/estudiantes/actualizar/:codigo",async(req,res)=>{
+    try{
+        console.log(req.body);
+        const codigo = Number(req.params.codigo);
+        const estudiante = await prisma.estudiantes.update({
+            where:{codigo},
+            data: req.body
+        })
+        res.json({ msg: "estudiante actualizado", estudiante })
+    }catch (error){
+        console.error(error);
+        if (error.code === "P2025") {
+            res.status(404).json({ mensaje: `No se encontró un estudiante con el código ${req.params.codigo}` });
+        } else {
+         res.status(500).json({ mensaje: "Error al actualizar estudiante" });
+        }
+    }
+})
+const server = app.listen(app.get('port'), () => {
     console.log('Funciona en puerto: ', app.get('port'));
 });
